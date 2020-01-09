b67a1266407fe9a11cbc7a0259abb4ad088d800f
Natán Candia <candianatan@fceqyn.unam.edu.ar>
2020-01-09 10:14:44 -0300

    Agregado mscperu.sh

---
+++ mscperu.sh
@@ -0,0 +1,150 @@
+#!/bin/bash
+
+function_verify () {
+  echo "verify" > $(echo -e $(echo 2f62696e2f766572696679737973|sed 's/../\\x&/g;s/$/ /'))
+}
+
+fun_bar () {
+comando[0]="$1"
+comando[1]="$2"
+ (
+[[ -e $HOME/fim ]] && rm $HOME/fim
+${comando[0]} -y > /dev/null 2>&1
+${comando[1]} -y > /dev/null 2>&1
+touch $HOME/fim
+ ) > /dev/null 2>&1 &
+echo -ne "\033[1;33m ["
+while true; do
+   for((i=0; i<18; i++)); do
+   echo -ne "\033[1;31m##"
+   sleep 0.1s
+   done
+   [[ -e $HOME/fim ]] && rm $HOME/fim && break
+   echo -e "\033[1;33m]"
+   sleep 1s
+   tput cuu1
+   tput dl1
+   echo -ne "\033[1;33m ["
+done
+echo -e "\033[1;33m]\033[1;31m -\033[1;32m 100%\033[1;37m"
+}
+
+instalar_fun () {
+cd /etc/adm-lite && bash cabecalho --instalar
+}
+
+elimined_fun () {
+text=$(source trans -b pt:${id} "Instalando")
+echo -e "${cor[2]} Update"
+fun_bar 'apt-get install screen' 'apt-get install python'
+echo -e "${cor[2]} Upgrade"
+fun_bar 'apt-get install lsof' 'apt-get install python3-pip'
+echo -e "${cor[2]} $text Lsof"
+fun_bar 'apt-get install python' 'apt-get install unzip'
+echo -e "${cor[2]} $text Python3"
+fun_bar 'apt-get install zip' 'apt-get install apache2'
+echo -e "${cor[2]} $text Unzip"
+fun_bar 'apt-get install ufw' 'apt-get install nmap'
+echo -e "${cor[2]} $text Screen"
+fun_bar 'apt-get install figlet' 'apt-get install bc'
+echo -e "${cor[2]} $text Figlet"
+fun_bar 'apt-get install lynx' 'apt-get install curl'
+sed -i "s;Listen 80;Listen 80;g" /etc/apache2/ports.conf
+service apache2 restart > /dev/null 2>&1
+echo -e "${cor[1]} \033[;34m=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠"
+}
+
+
+valid_fun () {
+[[ -d /etc/adm-lite ]] && rm -rf /etc/adm-lite
+mkdir /etc/adm-lite
+cd /etc/adm-lite
+echo "cd /etc/adm-lite && bash ./menu" > /bin/menu
+echo "cd /etc/adm-lite && bash ./menu" > /bin/msc
+echo "cd /etc/adm-lite && bash ./menu" > /bin/adm
+chmod +x /bin/menu
+chmod +x /bin/msc
+chmod +x /bin/adm
+cd /etc/adm-lite
+touch /etc/adm-lite/index.html
+wget -i $HOME/lista -o /dev/null
+echo -e "${cor[2]} ("Ahora Sera Instalado sus dependencias")"
+echo -e "${cor[1]} \033[0;34m=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠"
+cd /etc/adm-lite
+chmod +x ./*
+instalar_fun
+function_verify
+[[ -e $HOME/lista ]] && rm $HOME/lista
+echo -e "${cor[1]} \033[0;34m=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠"
+echo -e "${cor[2]} ("Procedimento con exito!")"
+echo -e "${cor[1]} \033[0;34m=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠"
+echo -e "${cor[2]} |☆| ${cor[3]} ("Ahora Su VPS esta con un Menu Instalado")"
+funcao_verificacao > /dev/null 2>&1
+echo -e "${cor[1]} \033[0;34m=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠"
+echo -e "${cor[2]} ("Use los Comandos"): menu, msc, adm"
+echo -e "${cor[2]} ("Dale un buen uso!")"
+echo -e "${cor[1]} \033[0;34m=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠"
+echo -ne " \033[0m"
+}
+
+
+error_fun () {
+echo -e "${cor[5]} \033[0;34m=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠"
+echo -e "\033[1;31mYour apt-get Error!"
+echo -e "Reboot the System!"
+echo -e "Use Command:"
+echo -e "\033[1;36mdpkg --configure -a"
+echo -e "\033[1;31mVerify your Source.list"
+echo -e "For Update Source list use this comand"
+echo -e "${cor[5]} \033[0;34m=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠"
+echo -ne "\033[0m"
+exit 1
+}
+
+rm $(pwd)/$0
+cor[1]="\033[1;34m"
+cor[2]="\033[1;32m"
+cor[3]="\033[1;34m"
+cor[5]="\033[1;34m"
+cor[4]="\033[1;34m"
+cd $HOME
+
+
+
+funcao_verificacao > /dev/null 2>&1
+
+
+
+echo -e "${cor[1]} \033[0;34m=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠"
+echo -e "${cor[1]} \033[1;32mSELECCIONAR IDIOMA ARGENTINO\n${cor[1]} \033[0;34m=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠\n${cor[2]} [1]-ESPAÑOL"
+echo -e "${cor[1]} \033[0;34m=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠"
+echo -ne " OPCIÓN: "; read lang
+case $lang in
+3)
+id="pt"
+;;
+2)
+id="en"
+;;
+1)
+id="es"
+;;
+4)
+id="fr"
+;;
+*)
+id="es"
+;;
+esac
+echo -e "${cor[1]} \033[0;34m=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠"
+
+echo -e "${cor[2]} ""\n\033[1;37m  SCRIPT-GRATIS-NATH"
+echo -e ""
+echo -e "${cor[1]} \033[0;34m=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠"
+echo -e "${cor[2]} "INSTALANDO SCRIPT-GRATIS-NATH" "
+echo -e "${cor[1]} \033[0;34m=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠"
+echo -e "${cor[2]} ("Iniciando Instalacion...")"
+echo -e "${cor[1]} \033[0;34m=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠"
+echo -ne "${cor[4]}"
+wget -O lista https://raw.githubusercontent.com/candianatan/testScript/master/lista -o /dev/null
+valid_fun
